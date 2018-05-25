cf2507f0844d67358acce2a632c3de6783cd9a6d
Artur Diniz Adam <artur.daz@gmail.com>
2018-05-25 16:49:36 -0300

    Formatando dois dígitos tempo

--- apostilaIgnore/lib.js
+++ apostilaIgnore/lib.js
@@ -3,5 +3,5 @@ function formataRelogio(){
     var tempoEmHorasFloat = tempo/60
     var horas = parseInt(tempoEmHorasFloat)
     var minutos = parseInt((tempoEmHorasFloat - horas) * 60)
-    return horas + ":" + minutos
+    return horas.toString().padStart(2,0) + ":" + minutos.toString().padStart(2,0)
 }
